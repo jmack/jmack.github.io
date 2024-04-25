df4e8ebc7c567e9e0fc6e289963b98f6650627b0
James Mack <jmack.contact@gmail.com>
2024-04-25 14:27:44 -0500

    Add header, sidebar, basic stylings

--- src/components/component.module.ts
+++ src/components/component.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from 'src/app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { RosterGroupComponent } from './roster-group/roster-group.component';
-import { RosterSoldierComponent } from './roster-soldier/roster-soldier.component';
 import { RouterLink } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { RosterGroupComponent } from './roster-group/roster-group.component';
+import { RosterSoldierComponent } from './roster-soldier/roster-soldier.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 @NgModule({
-  declarations: [RosterGroupComponent, RosterSoldierComponent],
+  declarations: [
+    HeaderComponent,
+    RosterGroupComponent,
+    RosterSoldierComponent,
+    SidebarComponent,
+  ],
   imports: [BrowserModule, CommonModule, RouterLink],
-  exports: [RosterGroupComponent, RosterSoldierComponent],
+  exports: [
+    HeaderComponent,
+    RosterGroupComponent,
+    RosterSoldierComponent,
+    SidebarComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
